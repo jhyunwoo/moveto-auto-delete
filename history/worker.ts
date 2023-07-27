c1fbabaa989e5e0163b464fb96b485f0d8c24430
jhyunwoo <jhyunwoo0228@gmail.com>
2023-07-27 22:56:14 +0900

    migrate to r2

---
+++ history/worker.ts
@@ -0,0 +1,65 @@
+import PocketBase from 'pocketbase';
+
+export interface Env {
+	ADMIN_EMAIL: string;
+	ADMIN_PASSWORD: string;
+}
+
+export default {
+	async scheduled(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		async function deleteFiles() {
+			const pb = new PocketBase('https://api.moveto.kr');
+			const loginAdmin = await pb.admins.authWithPassword(env.ADMIN_EMAIL, env.ADMIN_PASSWORD);
+			if (!loginAdmin) {
+				const json = JSON.stringify({ message: 'auth error' }, null, 2);
+				return new Response(json, {
+					headers: {
+						'content-type': 'application/json;charset=UTF-8',
+					},
+				});
+			}
+
+			const records = await pb.collection('files').getFullList({ expand: 'user' });
+			const currentTime = new Date();
+
+			let deletedFile = [];
+
+			for (let i = 0; i < records.length; i += 1) {
+				if (records[i].files.length > 0 || records[i].link) {
+					let downloadTime = 5 * 60000;
+					const userInfo = records[i].expand.user;
+					if (userInfo !== undefined) {
+						//@ts-ignore
+						if (userInfo?.plan) {
+							//@ts-ignore
+							if (userInfo.plan === 'FREE') {
+								downloadTime = 10 * 60000;
+								//@ts-ignore
+							} else if (userInfo.plan === 'BASIC') {
+								downloadTime = 30 * 60000;
+								//@ts-ignore
+							} else if (userInfo.plan === 'PRO') {
+								downloadTime = 60 * 60000;
+							}
+						}
+					}
+					let createdDate = new Date(records[i].created);
+					const expireTime = new Date(createdDate.getTime() + downloadTime);
+					if (expireTime < currentTime) {
+						const deleteFile = await pb.collection('files').update(records[i].id, { files: [], accessCode: '' });
+						deletedFile.push(deleteFile);
+					}
+				}
+			}
+
+			const json = JSON.stringify(deletedFile, null, 2);
+
+			return new Response(json, {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+				},
+			});
+		}
+		ctx.waitUntil(deleteFiles());
+	},
+};
--- src/worker.ts
+++ src/worker.ts
@@ -1,59 +1,80 @@
-import PocketBase from 'pocketbase';
-
 export interface Env {
-	ADMIN_EMAIL: string;
-	ADMIN_PASSWORD: string;
+	CLOUDFLARE_ACCOUNT_ID: string;
+	R2_ACCESSKEY: string;
+	R2_PRIVITEKEY: string;
 }
 
+type TargetDataType = {
+	fileKeys: string[];
+};
+
+import { S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
+
 export default {
-	async scheduled(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const targetFiles = await fetch('http://localhost:3000/api/share/delete', {
+			method: 'PUT',
+			body: JSON.stringify({ id: 'jhyunwoo0228@gmail.com', password: 'bcKboZsfgt5XRW7rRbxk' }),
+		});
+		const targetData: TargetDataType = await targetFiles.json();
+
+		const S3 = new S3Client({
+			region: 'auto',
+			endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+			credentials: {
+				accessKeyId: env.R2_ACCESSKEY!,
+				secretAccessKey: env.R2_PRIVITEKEY!,
+			},
+		});
+
+		for (let i = 0; i < targetData.fileKeys.length; i += 1) {
+			try {
+				const input = {
+					// DeleteObjectRequest
+					Bucket: 'moveto-bucket', // required
+					Key: targetData.fileKeys[i], // required
+				};
+				const command = new DeleteObjectCommand(input);
+				await S3.send(command);
+			} catch {}
+		}
+		const json = JSON.stringify(targetData, null, 2);
+		return new Response(json, {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+			},
+		});
+	},
+	async scheduled(request: Request, env: Env, ctx: ExecutionContext) {
 		async function deleteFiles() {
-			const pb = new PocketBase('https://api.moveto.kr');
-			const loginAdmin = await pb.admins.authWithPassword(env.ADMIN_EMAIL, env.ADMIN_PASSWORD);
-			if (!loginAdmin) {
-				const json = JSON.stringify({ message: 'auth error' }, null, 2);
-				return new Response(json, {
-					headers: {
-						'content-type': 'application/json;charset=UTF-8',
-					},
-				});
+			const targetFiles = await fetch('https://www.moveto.kr/api/share/delete', {
+				method: 'PUT',
+				body: JSON.stringify({ id: 'jhyunwoo0228@gmail.com', password: 'bcKboZsfgt5XRW7rRbxk' }),
+			});
+			const targetData: TargetDataType = await targetFiles.json();
+
+			const S3 = new S3Client({
+				region: 'auto',
+				endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+				credentials: {
+					accessKeyId: env.R2_ACCESSKEY!,
+					secretAccessKey: env.R2_PRIVITEKEY!,
+				},
+			});
+
+			for (let i = 0; i < targetData.fileKeys.length; i += 1) {
+				console.log(targetData.fileKeys[i]);
+				try {
+					const input = {
+						// DeleteObjectRequest
+						Bucket: 'moveto-bucket', // required
+						Key: targetData.fileKeys[i], // required
+					};
+					const command = new DeleteObjectCommand(input);
+					await S3.send(command);
+				} catch {}
 			}
-
-			const records = await pb.collection('files').getFullList({ expand: 'user' });
-			const currentTime = new Date();
-
-			let deletedFile = [];
-
-			for (let i = 0; i < records.length; i += 1) {
-				if (records[i].files.length > 0 || records[i].link) {
-					let downloadTime = 5 * 60000;
-					const userInfo = records[i].expand.user;
-					if (userInfo !== undefined) {
-						//@ts-ignore
-						if (userInfo?.plan) {
-							//@ts-ignore
-							if (userInfo.plan === 'FREE') {
-								downloadTime = 10 * 60000;
-								//@ts-ignore
-							} else if (userInfo.plan === 'BASIC') {
-								downloadTime = 30 * 60000;
-								//@ts-ignore
-							} else if (userInfo.plan === 'PRO') {
-								downloadTime = 60 * 60000;
-							}
-						}
-					}
-					let createdDate = new Date(records[i].created);
-					const expireTime = new Date(createdDate.getTime() + downloadTime);
-					if (expireTime < currentTime) {
-						const deleteFile = await pb.collection('files').update(records[i].id, { files: [], accessCode: '' });
-						deletedFile.push(deleteFile);
-					}
-				}
-			}
-
-			const json = JSON.stringify(deletedFile, null, 2);
-
+			const json = JSON.stringify(targetData, null, 2);
 			return new Response(json, {
 				headers: {
 					'content-type': 'application/json;charset=UTF-8',
